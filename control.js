2f810dfabe0dd73814bc3c8ed058c638dc2e391a
Eli Weber <eliweber2001@gmail.com>
2021-09-10 22:03:33 -0400

    Loading screen

--- control.js
+++ control.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", quitAnimation);
+
 function openPage(opener)
 {
     let myPage = document.getElementsByClassName(opener);
@@ -8,4 +10,10 @@ function closePage(closer)
 {
     let myPage = document.getElementsByClassName(closer);
     myPage[0].style.display = "none";
+}
+
+function quitAnimation()
+{
+    let closer = document.getElementsByClassName("animationPage");
+    closer[0].style.display = "none";
 }
